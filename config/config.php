7cf48ea19fa360bc1886fbb5868c525991264fd3
cmb69 <cmbecker69@gmx.de>
2012-08-23 21:08:38 +0000

    removed superfluous options from config.php

--- config/config.php
+++ config/config.php
@@ -1,20 +1,6 @@
 <?php
 
-$plugin_cf['codeeditor']['utf-8-marker']="äöüß";
-
 $plugin_cf['codeeditor']['enabled']="1";
 $plugin_cf['codeeditor']['theme']="default";
-$plugin_cf['codeeditor']['indent_unit']="4";
-$plugin_cf['codeeditor']['tab_size']="8";
-$plugin_cf['codeeditor']['indent_with_tabs']="1";
-$plugin_cf['codeeditor']['tab_mode']="shift";
-$plugin_cf['codeeditor']['electric_chars']="1";
-$plugin_cf['codeeditor']['line_wrapping']="1";
-$plugin_cf['codeeditor']['line_numbers']="1";
-$plugin_cf['codeeditor']['first_line_number']="1";
-$plugin_cf['codeeditor']['match_brackets']="1";
-$plugin_cf['codeeditor']['work_time']="200";
-$plugin_cf['codeeditor']['work_delay']="300";
-$plugin_cf['codeeditor']['undo_depth']="40";
 
 ?>
