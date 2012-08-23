fcc6537415b4c33fa5313d1688bb8bd6c4b9e5c2
cmb69 <cmbecker69@gmx.de>
2012-08-23 22:26:27 +0000

    cleaned up admin.php

--- admin.php
+++ admin.php
@@ -7,16 +7,14 @@
  */
 
 
-// utf-8-marker: äöüß
-
-
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
-    exit;
+    header('Content-Type: text/plain; charset=utf-8');
+    exit('Access forbidden');
 }
 
 
-define('CODEEDITOR_VERSION', '1beta1');
+define('CODEEDITOR_VERSION', '1beta2');
 
 
 /**
@@ -24,24 +22,25 @@ define('CODEEDITOR_VERSION', '1beta1');
  *
  * @return string  The (X)HTML.
  */
-function codeeditor_version() {
-    return '<h1>Codeeditor_XH</h1>'."\n"
-	    .'<p>Version: '.CODEEDITOR_VERSION.'</p>'."\n"
-	    .'<p><a href="http://3-magi.net/?CMSimple_XH/Codeeditor_XH">Codeeditor_XH</a> is powered by '
-	    .'<a href="http://codemirror.net/" target="_blank">'
-	    .'CodeMirror</a>.</p>'."\n"
-	    .'<p>Copyright &copy; 2011-2012 <a href="http://3-magi.net">Christoph M. Becker</a></p>'."\n"
-	    .'<p style="text-align:justify">This program is free software: you can redistribute it and/or modify'
-	    .' it under the terms of the GNU General Public License as published by'
-	    .' the Free Software Foundation, either version 3 of the License, or'
-	    .' (at your option) any later version.</p>'."\n"
-	    .'<p style="text-align:justify">This program is distributed in the hope that it will be useful,'
-	    .' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	    .' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	    .' GNU General Public License for more details.</p>'."\n"
-	    .'<p style="text-align:justify">You should have received a copy of the GNU General Public License'
-	    .' along with this program.  If not, see'
-	    .' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>'."\n";
+function codeeditor_version()
+{
+    return '<h1>Codeeditor_XH</h1>'
+	. '<p>Version: '.CODEEDITOR_VERSION.'</p>'
+	. '<p><a href="http://3-magi.net/?CMSimple_XH/Codeeditor_XH">Codeeditor_XH</a> is powered by '
+	. '<a href="http://codemirror.net/" target="_blank">'
+	. 'CodeMirror</a>.</p>'
+	. '<p>Copyright &copy; 2011-2012 <a href="http://3-magi.net">Christoph M. Becker</a></p>'
+	. '<p style="text-align:justify">This program is free software: you can redistribute it and/or modify'
+	. ' it under the terms of the GNU General Public License as published by'
+	. ' the Free Software Foundation, either version 3 of the License, or'
+	. ' (at your option) any later version.</p>'
+	. '<p style="text-align:justify">This program is distributed in the hope that it will be useful,'
+	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
+	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+	. ' GNU General Public License for more details.</p>'
+	. '<p style="text-align:justify">You should have received a copy of the GNU General Public License'
+	. ' along with this program.  If not, see'
+	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
 }
 
 
@@ -50,35 +49,36 @@ function codeeditor_version() {
  *
  * @return string  The (X)HTML.
  */
-function codeeditor_system_check() { // RELEASE-TODO
+function codeeditor_system_check() // RELEASE-TODO
+{
     global $pth, $tx, $plugin_tx, $plugin_cf;
 
     define('CODEEDITOR_PHP_VERSION', '4.0.7');
-    $ptx =& $plugin_tx['codeeditor'];
-    $imgdir = $pth['folder']['plugins'].'codeeditor/images/';
-    $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
-    $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
-    $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
-    $htm = tag('hr').'<h4>'.$ptx['syscheck_title'].'</h4>'
-	    .(version_compare(PHP_VERSION, CODEEDITOR_PHP_VERSION) >= 0 ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.sprintf($ptx['syscheck_phpversion'], CODEEDITOR_PHP_VERSION)
-	    .tag('br').tag('br')."\n";
+    $ptx = $plugin_tx['codeeditor'];
+    $imgdir = $pth['folder']['plugins'] . 'codeeditor/images/';
+    $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
+    $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
+    $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
+    $o = tag('hr') . '<h4>' . $ptx['syscheck_title'] . '</h4>'
+	. (version_compare(PHP_VERSION, CODEEDITOR_PHP_VERSION) >= 0 ? $ok : $fail)
+	. '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], CODEEDITOR_PHP_VERSION)
+	. tag('br') . tag('br');
     foreach (array() as $ext) {
-	$htm .= (extension_loaded($ext) ? $ok : $fail)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_extension'], $ext).tag('br')."\n";
+	$o .= (extension_loaded($ext) ? $ok : $fail)
+	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext) . tag('br');
     }
-    $htm .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_encoding'].tag('br')."\n";
-    $htm .= (!get_magic_quotes_runtime() ? $ok : $warn)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_magic_quotes'].tag('br').tag('br')."\n";
+    $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
+	. '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br');
+    $o .= (!get_magic_quotes_runtime() ? $ok : $warn)
+	. '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br') . tag('br');
     foreach (array('config/', 'css/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'].'codeeditor/'.$folder;
+	$folders[] = $pth['folder']['plugins'].'codeeditor/' . $folder;
     }
     foreach ($folders as $folder) {
-	$htm .= (is_writable($folder) ? $ok : $warn)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_writable'], $folder).tag('br')."\n";
+	$o .= (is_writable($folder) ? $ok : $warn)
+	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder) . tag('br');
     }
-    return $htm;
+    return $o;
 }
 
 
@@ -110,36 +110,33 @@ function codeeditor() {
     }
 
     if ($mode) {
-	$dir = $pth['folder']['plugins'].'codeeditor/';
-	include_once $pth['folder']['plugins'].'codeeditor/init.php';
+	$dir = $pth['folder']['plugins'] . 'codeeditor/';
+	include_once $pth['folder']['plugins'] . 'codeeditor/init.php';
 	include_codeeditor();
-	$config = codeeditor_config($mode);
-	$onload .= 'CODEEDITOR.instantiateByClasses(\''.$class.'\', '.$config.', false);';
+	$config = codeeditor_config($mode, '');
+	$onload .= "codeeditor.instantiateByClasses('$class', $config, false);";
     }
 }
 
 
-/**
- * Plugin administration.
+/*
+ * Handle plugin administration.
  */
-if (!empty($codeeditor)) {
-    initvar('admin');
-    initvar('action');
-
+if (isset($codeeditor) && $codeeditor == 'true') {
     $o .= print_plugin_admin('off');
 
     switch ($admin) {
-	case '':
-	    $o .= codeeditor_version().codeeditor_system_check();
-	    break;
-	default:
-	    $o .= plugin_admin_common($action, $admin, $plugin);
+    case '':
+	$o .= codeeditor_version() . codeeditor_system_check();
+	break;
+    default:
+	$o .= plugin_admin_common($action, $admin, $plugin);
     }
 }
 
 
-/**
- * Activates CodeMirror.
+/*
+ * Activate CodeMirror.
  */
 if ($plugin_cf['codeeditor']['enabled']) {
     codeeditor();
