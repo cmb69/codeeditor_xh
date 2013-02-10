1736d5b4f434e4a370d7feb4803100dc3dd9a8e0
cmb69 <cmbecker69@gmx.de>
2013-02-10 22:51:42 +0000

    added Czech and Slovak localization (contributed by oldnema resp. Tata)

---
+++ languages/cs.php
@@ -0,0 +1,13 @@
+<?php
+
+$plugin_tx['codeeditor']['syscheck_title']="Kontrola systému check";
+$plugin_tx['codeeditor']['syscheck_phpversion']="PHP verze ≥ %s";
+$plugin_tx['codeeditor']['syscheck_extension']="Rozšíření '%s' načteno";
+$plugin_tx['codeeditor']['syscheck_encoding']="Kódování 'UTF-8' nastaveno";
+$plugin_tx['codeeditor']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['codeeditor']['syscheck_writable']="Do adresáře '%s' lze zapisovat";
+$plugin_tx['codeeditor']['confirm_leave']="Vaše úpravy se ztratí!";
+$plugin_tx['codeeditor']['cf_enabled']="Má se Codeeditor_XH použít i na editování šablon a stylů? \"1\" (ano) nebo \"0\" (ne)";
+$plugin_tx['codeeditor']['cf_theme']="Téma zvýrazňování syntaxe: \"default\", \"ambiance\", \"ambiance-mobile\", \"blackboard\", \"cobalt\", \"eclipse\", \"elegant\", \"erlang-dark\", \"ge\", \"lesser-dark\", \"monokai\", \"neat\", \"night\", \"rubyblue\", \"twilight\", \"vibrant-ink\" or \"xq-dark\". Leave blank to disable syntax highlighting.";
+
+?>
