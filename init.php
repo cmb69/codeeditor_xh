3a7f341e4e3fbcfc476b4077f26b20a5154225ee
Christoph M. Becker <cmbecker69@gmx.de>
2014-12-22 18:34:45 +0100

    simplified init.php

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 /**
  * General editor interface of Codeeditor_XH.
  *
- * PHP versions 4 and 5
+ * PHP version 5
  *
  * @category  CMSimple_XH
  * @package   Codeeditor
@@ -12,22 +12,6 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Codeeditor_XH
  */
-
-/*
- * Prevent direct access and usage from unsupported CMSimple_XH versions.
- */
-if (!defined('CMSIMPLE_XH_VERSION')
-    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
-    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6', 'lt')
-) {
-    header('HTTP/1.1 403 Forbidden');
-    header('Content-Type: text/plain; charset=UTF-8');
-    die(<<<EOT
-Codeeditor_XH detected an unsupported CMSimple_XH version.
-Uninstall Codeeditor_XH or upgrade to a supported CMSimple_XH version!
-EOT
-    );
-}
 
 require_once $pth['folder']['plugin_classes'] . 'Controller.php';
 
@@ -84,17 +68,6 @@ function init_codeeditor($classes = array(), $config = false)
 {
 // @codingStandardsIgnoreEnd
     return Codeeditor_Controller::init($classes, $config);
-}
-
-/*
- * Include config and language file, if not yet done.
- */
-global $sl; // file can be included from within a function
-if (!isset($cf['codeeditor'])) {
-    include $pth['folder']['plugins'] . 'codeeditor/config/config.php';
-}
-if (!isset($tx['codeeditor'])) {
-    include $pth['folder']['plugins'] . 'codeeditor/languages/' . $sl . '.php';
 }
 
 ?>
