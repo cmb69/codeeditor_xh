096b4e52014212884bab320488f77e2eda808e2b
Christoph M. Becker <cmbecker69@gmx.de>
2014-12-23 01:17:58 +0100

    fixed config help texts wrt. metaconfig.php

--- languages/en.php
+++ languages/en.php
@@ -13,7 +13,7 @@ $plugin_tx['codeeditor']['confirm_leave']="Your modifications will be lost!";
 
 $plugin_tx['codeeditor']['error_json']="Invalid Codeeditor_XH configuration:";
 
-$plugin_tx['codeeditor']['cf_enabled']="Whether Codeeditor_XH is enabled for editing of template and stylesheets: \"1\" (yes) or \"0\" (no)";
-$plugin_tx['codeeditor']['cf_theme']="The syntax highlighting theme: \"default\", \"ambiance\", \"ambiance-mobile\", \"blackboard\", \"cobalt\", \"eclipse\", \"elegant\", \"erlang-dark\", \"ge\", \"lesser-dark\", \"monokai\", \"neat\", \"night\", \"rubyblue\", \"twilight\", \"vibrant-ink\" or \"xq-dark\". Leave blank to disable syntax highlighting.";
+$plugin_tx['codeeditor']['cf_enabled']="Whether Codeeditor_XH is enabled for editing of template and stylesheets.";
+$plugin_tx['codeeditor']['cf_theme']="The syntax highlighting theme.";
 
 ?>
--- languages/sk.php
+++ languages/sk.php
@@ -5,7 +5,7 @@ $plugin_tx['codeeditor']['syscheck_phpversion']="PHP verzia ≥ %s";
 $plugin_tx['codeeditor']['syscheck_extension']="Rozšírenie '%s' načítané";
 $plugin_tx['codeeditor']['syscheck_writable']="Do adresára '%s' možno zapisovať";
 $plugin_tx['codeeditor']['confirm_leave']="Vaše úpravy sa stratia!";
-$plugin_tx['codeeditor']['cf_enabled']="Má sa Codeeditor_XH použiť aj na editovanie šablón a štýlov? \"1\" (áno) alebo \"0\" (nie)";
-$plugin_tx['codeeditor']['cf_theme']="Téma zvýrazňovania syntaxe: \"default\", \"ambiance\", \"ambiance-mobile\", \"blackboard\", \"cobalt\", \"eclipse\", \"elegant\", \"erlang-dark\", \"ge\", \"lesser-dark\", \"monokai\", \"neat\", \"night\", \"rubyblue\", \"twilight\", \"vibrant-ink\" or \"xq-dark\". Leave blank to disable syntax highlighting.";
+$plugin_tx['codeeditor']['cf_enabled']="Má sa Codeeditor_XH použiť aj na editovanie šablón a štýlov?";
+$plugin_tx['codeeditor']['cf_theme']="Téma zvýrazňovania syntaxe.";
 
 ?>
