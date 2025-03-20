49cb34148b2c50ddae339a811de1af836739ad56
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-20 14:15:15 +0100

    Add more Editor tests

--- tests/EditorTest.php
+++ tests/EditorTest.php
@@ -8,6 +8,26 @@ use Plib\View;
 
 class EditorTest extends TestCase
 {
+    public function testIncludesEditor(): void
+    {
+        global $hjs;
+
+        $hjs = "";
+        $sut = new Editor("../", "night", "", $this->view());
+        $sut->doInclude();
+        Approvals::verifyHtml($hjs);
+    }
+
+    public function testInitializesEditor(): void
+    {
+        global $hjs, $bjs;
+
+        $hjs = $bjs = "";
+        $sut = new Editor("../", "default", "", $this->view());
+        $sut->init();
+        Approvals::verifyHtml($bjs);
+    }
+
     public function testReplacesTextarea(): void
     {
         $sut = new Editor("../", "default", "", $this->view());
