12bb28f4abc589cef207971327ac235f3565e105
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-21 13:36:16 +0100

    Back to dev

--- admin.php
+++ admin.php
@@ -27,7 +27,7 @@ if (!defined("CMSIMPLE_XH_VERSION")) {
     exit;
 }
 
-const CODEEDITOR_VERSION = "2.1";
+const CODEEDITOR_VERSION = "2.2-dev";
 
 /** @return list<string> */
 function codeeditor_getThemes(): array
