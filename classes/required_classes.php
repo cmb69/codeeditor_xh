0313675867ff539ad62df84e7585871c8c93e90d
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-26 15:31:48 +0200

    Back to dev

--- classes/required_classes.php
+++ classes/required_classes.php
@@ -22,7 +22,7 @@
 use Codeeditor\Dic;
 use Plib\Request;
 
-const CODEEDITOR_VERSION = "2.3";
+const CODEEDITOR_VERSION = "2.4-dev";
 
 // the 4 editor functions are public API; don't (re)move
 
--- tests/InfoCommandTest.php
+++ tests/InfoCommandTest.php
@@ -17,7 +17,7 @@ class InfoCommandTest extends TestCase
             new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["codeeditor"])
         );
         $response = $sut();
-        $this->assertSame("Codeeditor 2.3", $response->title());
+        $this->assertSame("Codeeditor 2.4-dev", $response->title());
         Approvals::verifyHtml($response->output());
     }
 
